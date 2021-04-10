676f850829bdfbe9d7ab2b59e6f031baaa53b8e8
regromrob <romae31@gmail.com>
2021-04-10 11:52:51 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,84 @@
+const express= require('express') 
+const app = express () 
+const MongoClient=require('mongodb').MongoClient 
+const PORT =2121 //
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING, 
+    dbName ='todo' 
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+    .then(client => { 
+        console.log(`Hey, connected to ${dbName} database`)
+        db= client.db(dbName) 
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+
+app.set('view engine', 'ejs') 
+app.use(express.static('public')) 
+app.use(express.urlencoded({extended:true })) 
+app.use(express.json()) 
+
+
+app.get('/', async (req, res)=>{ 
+    const todoItems = await db.collection('todos').find().toArray()
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) 
+    res.render('index.ejs', {zebra: todoItems, left: itemsLeft}) 
+    
+    })
+
+
+app.post('/createTodo', (req, res)=>{                  
+db.collection('todos').insertOne({ todo: req.body.todoItem, completed: false}) 
+    .then(result=> {
+        console.log('Todo has been added!')
+        res.redirect('/')
+    })
+})
+
+
+app.put('/markComplete', (req, res)=>{
+    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+        $set: {
+            completed: true
+        }
+    })
+    .then(result=>{
+        console.log('Marked completed')
+        res.json('Marked complete')
+    })
+})
+
+
+app.put('/undo', (req, res)=>{
+    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+        $set: {
+            completed: false
+        }
+    })
+    .then(result=>{
+        console.log('Marked completed')
+        res.json('Marked complete')
+    })
+})
+
+
+app.delete('/deleteTodo', (req, res)=>{ 
+db.collection('todos').deleteOne({todo: req.body.rainbowUnicorn})
+    .then(result=> {
+        console.log('Deleted todo')
+        res.json('Deleted it') 
+    })
+})
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
